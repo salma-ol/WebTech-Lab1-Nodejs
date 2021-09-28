445911facb4db8729195bb75efea6679155a9e02
salma-ol <khoffisalma@yahoo.fr>
2021-09-28 15:46:38 +0200

    Part 1 to Part 4

---
+++ handles.js
@@ -0,0 +1,30 @@
+const url = require('url')
+const qs = require('querystring')
+module.exports = {
+    serverHandle : function (req, res) {
+        const route = url.parse(req.url)
+        const path = route.pathname 
+        const params = qs.parse(route.query)
+        const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
+  
+      
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+      
+        if (path === '/hello/' && 'name' in params) {
+          res.write('Hello ' + params['name'])
+        } else {
+          res.write('Hello anonymous')
+        }
+        
+        res.end();
+      }
+  }
